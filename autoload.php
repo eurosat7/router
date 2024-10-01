6c81f3d9b3d18f7b94f981e814502e727a8f89e5
eurosat7 <eurosat7@yahoo.de>
2024-10-01 17:59:10 +0200

    composer is setup to look into "src" for some files to autoload. In case you do not want that I have a simple unoptimized loader. But please get composer set up!

---
+++ autoload.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+// get composer up and delete me!
+
+include __DIR__ . '/src/dto/Config.php';
+include __DIR__ . '/src/Routing/ParsedRoute.php';
+include __DIR__ . '/src/Routing/RouteFinder.php';
